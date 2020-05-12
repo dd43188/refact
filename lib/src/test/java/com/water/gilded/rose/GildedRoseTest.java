d5220df7bab934d0fa47395909d4b57ec6360c1d
jiamiaohe <jiamiaohe@tencent.com>
2020-05-12 16:50:03 +0800

    留一个案例用来说明覆盖率

--- lib/src/test/java/com/water/gilded/rose/GildedRoseTest.java
+++ lib/src/test/java/com/water/gilded/rose/GildedRoseTest.java
@@ -10,14 +10,14 @@ public class GildedRoseTest {
 
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdate,
-                new String[]{"foo", "Aged Brie", "Sulfuras, Hand of Ragnaros", "hjm", "Backstage passes to a TAFKAL80ETC concert"},
+                new String[]{"foo", "Aged Brie", "Sulfuras, Hand of Ragnaros", "hjm", /**"Backstage passes to a TAFKAL80ETC concert"**/},
                 new Integer[]{-1, 0, 5, 6, 10, 11},
                 new Integer[]{0, 1, 49, 50});
     }
 
     private String doUpdate(String name, int sellIn, int quality) {
         Item[] items = new Item[]{new Item(name, sellIn, quality)};
-        GildedRose1 app = new GildedRose1(items);
+        GildedRose app = new GildedRose(items);
         app.updateQuality();
         return items[0].toString();
     }
